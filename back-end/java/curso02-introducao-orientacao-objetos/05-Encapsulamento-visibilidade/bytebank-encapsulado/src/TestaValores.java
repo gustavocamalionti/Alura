8ff81c7b6cbcac74d48f608b2cc484edc5d4e64d
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-03-02 22:59:19 -0300

    Pequenas alterações

--- back-end/java/curso02-introducao-orientacao-objetos/05-Encapsulamento-visibilidade/bytebank-encapsulado/src/TestaValores.java
+++ back-end/java/curso02-introducao-orientacao-objetos/05-Encapsulamento-visibilidade/bytebank-encapsulado/src/TestaValores.java
@@ -12,7 +12,7 @@ public class TestaValores {
         System.out.println("Número da conta - Usando Setters: " + conta.getNumero()); //Mesma situação acima, são valores default (CONSTRUTOR).
 
         Conta conta2 = new Conta(1337,1145424196);
-        Conta conta3 = new Conta(1337,5113224791);
+        Conta conta3 = new Conta(1337,2113224791);
 
         System.out.println(Conta.getTotal());
 
