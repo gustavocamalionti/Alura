2555aee7f33297e240872f21a56a0ea18d490ad3
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-03-03 00:12:42 -0300

    Criando exemplos não funcionais e inviáveis

--- back-end/java/JOB03-java-polimorfismo-heranca-interfaces/01-introducao-heranca/bytebank-herdado/src/TesteFuncionarioErrado.java
+++ back-end/java/JOB03-java-polimorfismo-heranca-interfaces/01-introducao-heranca/bytebank-herdado/src/TesteFuncionarioErrado.java
@@ -4,5 +4,17 @@ public class TesteFuncionarioErrado {
         f1.setSalario(3000.0);
         System.out.println(f1.getTipo());
         System.out.println(f1.getBonificacao());
+
+        FuncionarioTesteErrado f2 = new FuncionarioTesteErrado();
+        f2.setTipo(1);
+        f2.setSalario(5000.0);
+        System.out.println(f2.getTipo());
+        System.out.println(f2.getBonificacao());
+
+        FuncionarioTesteErrado f3 = new FuncionarioTesteErrado();
+        f2.setTipo(2);
+        f2.setSalario(10000.0);
+        System.out.println(f2.getTipo());
+        System.out.println(f2.getBonificacao());
     }
 }
