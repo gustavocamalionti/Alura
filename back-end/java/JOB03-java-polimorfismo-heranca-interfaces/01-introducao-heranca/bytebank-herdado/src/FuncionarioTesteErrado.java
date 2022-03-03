2c71bfdefdda75c07a85d60ad1f43e171b7f89ba
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-03-03 00:00:53 -0300

    Criando FuncionarioTeste - Exemplo não usual e errado!

---
+++ back-end/java/JOB03-java-polimorfismo-heranca-interfaces/01-introducao-heranca/bytebank-herdado/src/FuncionarioTesteErrado.java
@@ -0,0 +1,39 @@
+public class FuncionarioTesteErrado {
+    private String nome;
+    private String cpf;
+    private double salario;
+
+    public FuncionarioTesteErrado() {
+
+    }
+
+    //Funcionario recebe uma bonificação de 10% do salario como padrão;
+    //Problema maior - Gerente e funcionario ganham bonificações diferentes. O que fazer? usar herança!
+    public double getBonificacao() {
+        return this.salario*0.1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
