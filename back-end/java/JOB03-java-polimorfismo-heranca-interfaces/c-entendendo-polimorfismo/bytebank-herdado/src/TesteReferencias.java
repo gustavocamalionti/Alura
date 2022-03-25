0a702711322a789771ddc91b853354e3694b4441
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-03-24 22:48:22 -0300

    Polimorfismo, o início

--- back-end/java/JOB03-java-polimorfismo-heranca-interfaces/c-entendendo-polimorfismo/bytebank-herdado/src/TesteReferencias.java
+++ back-end/java/JOB03-java-polimorfismo-heranca-interfaces/c-entendendo-polimorfismo/bytebank-herdado/src/TesteReferencias.java
@@ -1,10 +1,16 @@
 public class TesteReferencias {
     public static void main(String[] args) {
         Gerente g1 = new Gerente(); //Objeto Criado, declarando variável e atribuindo referência.
-        g1.setNome("Kleber");
-        String nome = g1.getNome();
+        g1.setNome("Kleber Matsumoto");
+        String nome1 = g1.getNome();
 
-        System.out.println(nome);
+        System.out.println(nome1);
+
+        Funcionario g2 = new Gerente(); //Como Funcionário é a classe mais genérica, podemos fazer isso.
+        g2.setNome("Gustavo Camalionti");
+        String nome2 = g2.getNome();
+
+        System.out.println(nome2);
 
     }
 }
