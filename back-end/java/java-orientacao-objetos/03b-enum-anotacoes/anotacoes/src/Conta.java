ca9bc1c84aac7e3f6990c0fe6c068af6e8f9e82b
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-04-04 23:50:42 -0300

    Anotacao e Enum

---
+++ back-end/java/java-orientacao-objetos/03b-enum-anotacoes/anotacoes/src/Conta.java
@@ -0,0 +1,70 @@
+
+@/**
+ *
+ *
+ *
+ * @author Gustavo Camalionti
+ * @version 1.0
+ */
+public abstract class Conta {
+    @DoublePositivo
+    protected double saldo;
+    private int agencia;
+    private int numero;
+
+    private static int total;
+    public Conta(int agencia, int numero) {
+
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public abstract void deposita(double valor);
+
+    public boolean saca(double valor){
+        if(this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if(this.saca(valor)) {
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() { return this.numero; }
+    public void setNumero(int numero) {
+        if (numero<=0) {
+            System.out.println("Não pode valor menor ou igual a 0 para Numero");
+            return;
+        }
+        this.numero = numero;
+    }
+
+    public int getAgencia() {return this.agencia;}
+    public void setAgencia(int agencia) {
+        if (agencia<=0) {
+            System.out.println("Não pode valor menor ou igual a 0 para Agencia");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public static int getTotal() { //DA CLASSE
+        return Conta.total;
+    }
+
+
+}
---
+++ back-end/java/java-orientacao-objetos/03b-enum-anotacoes/anotacoes/src/DoublePositivo.java
@@ -0,0 +1,9 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD) //Válido apenas em cima de um Atributo
+public @interface DoublePositivo {
+}
