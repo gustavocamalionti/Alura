3dc1ec697bd9cffeab2af7358b04147b2a868497
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-04-04 22:08:35 -0300

    edit

---
+++ back-end/java/JOB03-java-polimorfismo-heranca-interfaces/g-praticando-heranca-interface/bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,19 @@
+
+//Na herança, a gente herda os atributos e os métodos. No entanto, não herda os construtores.
+//new ContaCorrente()
+public class ContaCorrente extends Conta {
+    public ContaCorrente(int agencia, int numero) {
+        super(agencia, numero);
+    };
+
+    @Override //Anotação-configuração para o compilador
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valorASacar);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo = super.saldo + valor;
+    }
+}
