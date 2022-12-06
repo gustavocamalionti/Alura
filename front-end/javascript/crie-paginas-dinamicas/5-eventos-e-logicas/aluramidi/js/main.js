5cd9a0de0944ab67b637d47db562d1332158242c
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-12-06 17:23:52 -0300

    keydown

--- front-end/javascript/crie-paginas-dinamicas/5-eventos-e-logicas/aluramidi/js/main.js
+++ front-end/javascript/crie-paginas-dinamicas/5-eventos-e-logicas/aluramidi/js/main.js
@@ -6,31 +6,17 @@ function TocaSom(idElementoAudio){
 //Referências ----------------------------------------------------------------------------------------
 const listaDeTeclas = document.querySelectorAll('.tecla'); //o valor é constante
 
-
 //Para -------------------------------------------------------------------------------------------
 for (let contador=0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`
 
-    tecla.onclick = function () {
+    tecla.onclick = function() {
         TocaSom(idAudio);
     };
+
+    tecla.onkeydown = function() {
+        tecla.classList.add('ativa');
+    }
 }
-
-// //Enquanto -------------------------------------------------------------------------------------------
-//let contador = 0  //receber sempre um valor novo;
-// while (contador < listaDeTeclas.length) {
-//     const tecla = listaDeTeclas[contador];
-//     const instrumento = tecla.classList[1];
-
-//     //template string - pesquisar sobre
-//     const idAudio = `#som_${instrumento}`
-
-//     tecla.onclick = function () {
-//         TocaSom(idAudio);
-//     };
-
-//     contador = contador + 1;
-//     // console.log(contador);
-// }
