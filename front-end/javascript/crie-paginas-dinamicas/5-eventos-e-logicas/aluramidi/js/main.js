1f547a47666056087adc833fb27146ef3bf9718c
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-12-06 17:12:08 -0300

    preparando ambiente para o ultimo módulo do curso

---
+++ front-end/javascript/crie-paginas-dinamicas/5-eventos-e-logicas/aluramidi/js/main.js
@@ -0,0 +1,36 @@
+//Funções --------------------------------------------------------------------------------------------
+function TocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+//Referências ----------------------------------------------------------------------------------------
+const listaDeTeclas = document.querySelectorAll('.tecla'); //o valor é constante
+
+
+//Para -------------------------------------------------------------------------------------------
+for (let contador=0; contador < listaDeTeclas.length - 1; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        TocaSom(idAudio);
+    };
+}
+
+// //Enquanto -------------------------------------------------------------------------------------------
+//let contador = 0  //receber sempre um valor novo;
+// while (contador < listaDeTeclas.length) {
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+
+//     //template string - pesquisar sobre
+//     const idAudio = `#som_${instrumento}`
+
+//     tecla.onclick = function () {
+//         TocaSom(idAudio);
+//     };
+
+//     contador = contador + 1;
+//     // console.log(contador);
+// }
